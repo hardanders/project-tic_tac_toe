417c0cffec26bf901cc11269317414f4971ffd3e
hardanders <andyyy85@gmail.com>
2023-02-09 11:56:39 -0500

    Created input screen for names, adjusted button execution and player logic. TODO: Improve styles, add AI, alternate player turns

--- scripts.js
+++ scripts.js
@@ -1,43 +1,102 @@
-const Gameboard = (function() {  
-    const Player = () => {
+let player1, player2;
 
-    }
+const Gameboard = (function() {
+    const Player = (name, icon) => {
+        name = (() => {
+            if (player1 == undefined && !name) {
+                return "Player 1";
+            } else if (player1 != undefined && !name) {
+                return "Player 2";
+            } else {
+                return name;
+            }
+        })();
+
+        return { name, icon }
+    };
+
+    const assignPlayer = () => {
+        const p1Name = document.querySelector('#p1Name').value
+        const p2Name = document.querySelector('#p2Name').value
+        player1 = Player(p1Name, 'X')
+        player2 = Player(p2Name, 'O')
+    };
 
     const generateBoard = () => {
-        let board = []
-        const playArea = document.querySelector(".grid")
+        let board = [];
+        const playArea = document.querySelector(".grid");
         for (i = 0; i < 9; i++) {
-            let cls = ['tile', `a${[i]}`]
-            let box = document.createElement("div")
-            box.classList.add(...cls)
-            board.push(box)
-            playArea.append(board[i])
+            let cls = ['tile', `a${[i]}`];
+            let box = document.createElement("div");
+            box.classList.add(...cls);
+            board.push(box);
+            playArea.append(board[i]);
         }
     };
+
+    const inputIcon = (e) => {
+        e.target.innerHTML = currentPlayer.icon
+    };
+
     return {
-        generateBoard
-    }
+        generateBoard,
+        assignPlayer,
+        player1,
+        player2
+    };
 })();
 
 const DisplayController = (function() {
-    const hideHeader = (function() {
-        document.querySelector('.gameSelect').classList.toggle('hidden')
+
+    const toggleHeader = (function() {
+        const gameSelect = document.getElementsByClassName("gameSelect");
+        gameSelect[0].classList.toggle('hidden');
+    });
+
+    const toggleInputs = (function() {
+        const inputCon = document.getElementsByClassName("input-con");
+        inputCon[0].classList.toggle('hidden');
+    });
+
+    const toggleGamespace = (function() {
+        const gameSpace = document.getElementsByClassName("gameSpace");
+        gameSpace[0].classList.toggle('hidden');
+    });
+
+    const setNameDisplay = (function() {
+        document.querySelector('.p1Score').textContent = `${player1.name} : ${player1.icon}`;
+        document.querySelector('.p2Score').textContent = `${player2.name} : ${player2.icon}`;
     })
+
     const addListeners = (function() {
-        const tiles = document.querySelectorAll(".tile")
-        const playBtn = document.querySelector(".playBtn")
+        const tiles = document.querySelectorAll(".tile");
+        const playBtn = document.querySelector(".playBtn");
+        const submitBtn = document.querySelector('.submit');
         playBtn.addEventListener("click", function() {
-            hideHeader()
-            Gameboard.generateBoard()
-            addListeners()
+            toggleHeader();
+            toggleInputs();
         })
-        const playAIBtn = document.querySelector(".playAIBtn")
+
+        const playAIBtn = document.querySelector(".playAIBtn");
+
         for (i = 0; i < tiles.length; i++) {
             tiles[i].addEventListener("click", function() {
-                this.innerHTML = "X"
+                this.innerHTML = "X";
             })
         }
+
+        submitBtn.addEventListener('click', function(e) {
+            Gameboard.assignPlayer();
+            toggleInputs();
+            toggleGamespace();
+            Gameboard.generateBoard();
+            setNameDisplay();
+            addListeners();
+            e.preventDefault();
+        })
     });
+
+
     return {
         addListeners
     }
